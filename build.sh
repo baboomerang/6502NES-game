7219312e9e72861596a29cd866ebe90066cc1b07
baboomerang <irodrigue101@gmail.com>
2020-09-07 11:17:56 -0400

    updated build script to support asm6f

--- build.sh
+++ build.sh
@@ -10,13 +10,19 @@ else
 
     cd ${filepath}
 
-    if  nesasm "${filename}" | grep 'error'; then
-        echo "Errors occurred check below"
-        nesasm "${filename}"
-        exit 1
-    fi 
-
+    #if  nesasm "${filename}" | grep 'error'; then
+    #    echo "Errors occurred check below"
+    #    nesasm "${filename}"
+    #    exit 1
+    #fi
     #dumb solution but nesasm always returns 0 even if it errors
 
+    asm6f -c "${filename}.asm"
+    
+    if [ $? -ne 0 ]; then
+        echo "check errors"
+    fi
+
+    mv "${filename}.bin" "${filename}.nes"
     mesen "${filename}.nes"
 fi
