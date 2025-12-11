6c3d0e6dd95ae5d875d8ca14c5f52b9c77fb57c6
baboomerang <irodrigue101@gmail.com>
2025-12-10 20:19:40 -0500

    updated linker config and build task

--- main.s
+++ main.s
@@ -1,9 +1,16 @@
-.segment "HEADER"
-  ; .byte "NES", $1A      ; iNES header identifier
-  .byte $4E, $45, $53, $1A
-  .byte 2               ; 2x 16KB PRG code
-  .byte 1               ; 1x  8KB CHR data
-  .byte $01, $00        ; mapper 0, vertical mirroring
+.include "/src/include/nes2header.inc"
+
+;-------------------------------------------------
+; iNES header using nes2header.inc macros
+;-------------------------------------------------
+; These macros set internal variables within the nes2header.inc
+nes2mapper 0        ; Mapper 0 (NROM)
+nes2prg 32768       ; 2x16KB PRG ROM
+nes2chr 8192        ; 1x8KB CHR ROM
+nes2arrange 'V'     ; Vertical mirroring
+nes2tv 'N'          ; NTSC TV system
+
+nes2end
 
 .segment "VECTORS"
   ;; When an NMI happens (once per frame if enabled) the label nmi:
@@ -111,7 +118,7 @@ palettes:
   .byte $0f, $00, $00, $00
 
 ; Character memory
-.segment "CHARS"
+.segment "CHR"
   .byte %11000011	; H (00)
   .byte %11000011
   .byte %11000011
